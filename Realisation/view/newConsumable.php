ae82a19ea1e75f8eba156bbea3b97b172019b7c7
Michael Pedroletti <michael@pedroletti.ch>
2021-05-20 16:53:39 +0200

    Commencement de la mise en page pour les nouveaux consommables
    Début de la mise en page du formulaire d'ajout de nouveaux consommables

--- Realisation/view/newConsumable.php
+++ Realisation/view/newConsumable.php
@@ -7,6 +7,21 @@
 
 ob_start();
 ?>
+
+<div class="m-3 border border-dark pl-3 pr-3">
+    <h1 class="mt-3 text-center">Ajout de nouveaux consommables</h1>
+    <div class="mt-3 border-top border-dark d-flex flex-wrap w-100">
+        <div class="flex-column pt-5 pl-5 border-right border-dark w-50">
+            test
+        </div>
+        <div class="flex-column p-5 w-50">
+            test
+        </div>
+    </div>
+    <div class="w-100 text-center">
+        <button type="submit" class="btn btn-success w-25 p-2 m-3">Ajouter le nouveau consommable</button>
+    </div>
+</div>
 <h1>Page en construction ! Veuillez revenir plus tard !</h1>
 <?php
 $content = ob_get_clean();
